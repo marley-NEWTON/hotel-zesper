91d4a85d7779b0a7c8304127ae5c8bdd90a89604
marley-NEWTON <marleygeskhamis@gmail.com>
2026-04-28 15:56:40 +0300

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hotel-zesper/' // ⚠️ hakikisha ni jina sahihi la repo yako
 })
+
+
